006ba99fb9de320f9f3ff7a46b9c9b0a974b8d7e
Joel White <jwhite@sentryds.,com>
2015-12-10 16:02:47 -0500

    Adding information to the top of the scripts

--- Scripts/random_password.sh
+++ Scripts/random_password.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+################################################################
+#
+#	A Script to generate random passwords
+#
+#	Written by Joel E White
+#
+################################################################
+
 length=${1:-8}
 
 < /dev/urandom tr -dc A-Za-z0-9_\; | head -c${length}; echo
